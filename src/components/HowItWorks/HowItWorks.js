0a81d68ebbde3b959e62aab52af7957981938f43
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-04 16:06:42 -0800

    Add files via upload

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -6,13 +6,13 @@ export default function HowItWorks() {
         <div id="howitworks">
             <h2>How it works</h2>
             <img className="diagram" src={howItWorks} alt="HowItWorks" />
-            <img className="laptop" src={laptop} alt="laptop" />
-            <p>Do you want to easily access Stitchack?<br /> Click on the button below to download</p>
+            {/* <img className="laptop" src={laptop} alt="laptop" /> */}
+            {/* <p>Do you want to easily access Stitchack?<br /> Click on the button below to download</p>
             
             <span>
                 <button className="filled">Download for desktop</button>
                 <button className="outlined">Download for mobile</button>
-            </span>
+            </span> */}
 
         </div>
     )
