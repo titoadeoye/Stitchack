06a16327c1712af208f6ed885b04a392ec380eff
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-05 14:57:16 -0800

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,61 +6,113 @@ import { TwitterIcon, FacebookIcon, InstagramIcon, LinkedinIcon, EmailIcon } fro
 export default function Footer() {
     const [email, setEmail] = useState("")
     return (
-        <Wrapper>
-            <div>
-                <LogoWrap>
-                    <img src={logo} alt="logo" />
-                    <div className="socials">
-                        <TwitterIcon />
-                        <InstagramIcon />
-                        <FacebookIcon />
-                        <LinkedinIcon />
-                        <EmailIcon />
-                    </div>
-                </LogoWrap>
+        // <Wrapper>
+        // <div>
 
-                {/* <div className="links">
-                    <div>
-                        <h3>Who We Are</h3>
-                        <a href="#about">About us</a>
-                        <a href="#services">Our services</a>
-                        <a href="#home">Our Team</a>
-                        <a href="#home">Privacy Policy</a>
-                        <a href="#home">Terms of Service</a>
+        //     <LogoWrap>
+        //         <img src={logo} alt="logo" />
+        //         <div className="socials">
+        //             <TwitterIcon />
+        //             <InstagramIcon />
+        //             <FacebookIcon />
+        //             <LinkedinIcon />
+        //             <EmailIcon />
+        //         </div>
+        //     </LogoWrap>
 
-                    </div>
+        //     {/* <div className="links">
+        //         <div>
+        //             <h3>Who We Are</h3>
+        //             <a href="#about">About us</a>
+        //             <a href="#services">Our services</a>
+        //             <a href="#home">Our Team</a>
+        //             <a href="#home">Privacy Policy</a>
+        //             <a href="#home">Terms of Service</a>
 
-                    <div>
-                        <h3>Support</h3>
-                        <a href="#home">Get Started</a>
-                        <a href="#home">Help</a>
-                        <a href="#faq">FAQ</a>
+        //         </div>
 
-                    </div>
-                </div> */}
+        //         <div>
+        //             <h3>Support</h3>
+        //             <a href="#home">Get Started</a>
+        //             <a href="#home">Help</a>
+        //             <a href="#faq">FAQ</a>
 
-                <Copyrights>
-                    <p>@{new Date().getFullYear()} Stitchack | All rights reserved</p>
-                </Copyrights>
+        //         </div>
+        //     </div> */}
+
+        //     <Copyrights>
+        //         <p>@{new Date().getFullYear()} Stitchack | All rights reserved</p>
+        //     </Copyrights>
+        // </div>
+
+
+        // </Wrapper>
+        <Wrapper >
+            <div className="box mask">
+                <Text>
+
+                    <LogoWrap>
+                        <img src={logo} alt="logo" />
+                        <div className="socials">
+                            <TwitterIcon />
+                            <InstagramIcon />
+                            <FacebookIcon />
+                            <LinkedinIcon />
+                            <EmailIcon />
+                        </div>
+                    </LogoWrap>
+
+                    <Copyrights>
+                        <p>@{new Date().getFullYear()} Stitchack | All rights reserved</p>
+                    </Copyrights>
+                </Text>
             </div>
-
-
         </Wrapper>
     )
 }
-
 const Wrapper = styled.footer`
-    height: 50px;
-    background-color: black;
-    width: 100vw;
-
-    & > div {
+    .box {
+        --size: 50px;
+        height: 160px;
+        border: 1px solid;
         display: flex;
-        justify-content: space-between;
-        height: 100%;
-        padding: 0 10px;
+        
+        background:
+        radial-gradient(var(--size) at 50% var(--size),#6251C3 99%,#0000 101%) calc(50% - 2*var(--size)) 0/calc(4*var(--size)) 100% no-repeat,
+        radial-gradient(var(--size) at 50% 0px,#0000 99%,#A800AB 101%) 50% var(--size)/calc(4*var(--size)) 100% no-repeat;
     }
-`;
+    .box + .box {
+        background-repeat: repeat-x;
+    }
+    .box.mask {
+        -webkit-mask:
+        radial-gradient(var(--size) at 50% var(--size),#6251C3 99%,#0000 101%) calc(50% - 2*var(--size)) 0/calc(4*var(--size)) 100%,
+        radial-gradient(var(--size) at 50% 0px,#0000 99%,#A800AB 101%) 50% var(--size)/calc(4*var(--size)) 100% repeat-x;
+        background:linear-gradient(90deg,#A800AB,#6251C3);  
+        border: none;
+    }
+`
+// const Wrapper = styled.footer`
+//     height: 50px;
+//     background: linear-gradient(139.52deg,#6251C3 -73.08%,#A800AB 150.16%);
+//     width: 100vw;
+
+//     & > div {
+//         display: flex;
+//         justify-content: space-between;
+//         height: 100%;
+//         padding: 0 10px;
+//     }
+// `;
+
+const Text = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10px;
+    height: 60px;
+    width: 100%;
+    align-self: end;
+`
 
 const Copyrights = styled.div`
     
