5e4c3a9eb14fd6422cc9a7591ddf589b7927a4a2
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-04 16:03:22 -0800

    adding faq component

--- src/components/FAQs/FAQs.js
+++ src/components/FAQs/FAQs.js
@@ -1,7 +1,89 @@
-export default function FAQs () {
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel
+} from "react-accessible-accordion";
+import styled from "styled-components";
+
+export default function FAQs() {
+    const items = [
+        {
+            uuid: "1",
+            heading: "What does this website do?",
+            content: `Stitchack is a platform for tailors to manage their tasks, store 
+            customer data and back it up to the cloud and also track their revenue of a
+             period of time
+            
+            `
+        },
+        {
+            uuid: "2",
+            heading: "Is registration for free?",
+            content: `Yup! You can sign up right now for no money at all`
+        },
+        {
+            uuid: "3",
+            heading: "Can i store customers' measurements?",
+            content: `Definitely! You can store customer measurements and also back it up to the cloud.
+            Paper rots, the cloud is eternal.`
+        },
+    ]
     return (
-        <div>
-            <h1>Frequently asked questions</h1>
-        </div>
+        <Wrapper id="faqs">
+            <H2>FAQs</H2>
+            <Accordion allowZeroExpanded>
+            {items.map((item) => (
+                <StyledAccordionItem key={item.uuid}>
+                    <StyledAccordionItemHeading>
+                        <AccordionItemButton>
+                            {item.heading}
+                        </AccordionItemButton>
+                    </StyledAccordionItemHeading>
+                    <StyledAccordionItemPanel>
+                        {item.content}
+                    </StyledAccordionItemPanel>
+                </StyledAccordionItem>
+            ))}
+        </Accordion>
+        </Wrapper>
+        
     )
 }
+
+const Wrapper = styled.div`
+    width: 100%;
+    margin: auto;
+   text-align: center;
+    padding: 20px 0;
+`;
+
+const H2 = styled.h2`
+
+    margin: 2em;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 35px;
+    color: #000000;
+`;
+
+const StyledAccordionItem = styled(AccordionItem)`
+    // padding: 1.5em;
+    // width: 50em;
+    // background-color: rgb(250, 250, 250);
+    margin: 0.8em;
+`
+
+const StyledAccordionItemHeading = styled(AccordionItemHeading)`
+    padding: 1.5em;
+    width: 50em;
+    background-color: rgb(250, 250, 250);
+    margin: auto;
+`;
+
+const StyledAccordionItemPanel = styled(AccordionItemPanel)`
+    padding: 1.5em;
+    width: 50em;
+    margin: auto;
+`
