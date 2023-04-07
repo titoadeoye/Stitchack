80ad3586e0bf64c0be514b8d3b2270704efc219e
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-07 03:47:25 -0800

    Add files via upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import {bg} from "../../assets";
 import "./Home.css";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     return (
         <div id="home">
             <section>
-                <div  style={{display: "flex", justifyContent: "space-between"}}>
+                <div  className="home-content">
                    <div className="text">
                     <h1>Let us assist you in stress-free customer management</h1>
                     <p>We will assist you in keeping track of your customer's measurements
@@ -14,33 +15,12 @@ export default function Home() {
                     </p>
                     <button onClick={() => navigate("register")}>Get started</button>
                 </div>
-                {/* <img style={{marginRight: "2em", width: "200px", height: "200px",mixBlendMode: ""}} className="landing" src={mannequin} alt="landing" /> */}
+                <img className="landing" src={bg} alt="landing" />
   
                 </div>
                
                 <div class="curve"></div>
             </section>
         </div>
-
-        // <div id="home">
-        // {/* <div className="text">
-        //     <h1>Let us assist you in stress-free customer management</h1>
-        //     <p>We will assist you in keeping track of your customer's measurements
-        //         , details, and styles so that you can readily access them from any location at any time.
-        //     </p>
-        //     <button onClick={() => navigate("register")}>Get started</button>
-        //     <div className="brands">
-        //         <img src={armani} alt="brand" />
-        //         <img src={chanel} alt="brand" />
-        //         <img src={fendi} alt="brand" />
-        //         <img src={dolce} alt="brand" />
-
-        //     </div>
-
-        // </div>
-
-        // <img className="landing" src={landing} alt="landing" /> */}
-
-
     )
 }
