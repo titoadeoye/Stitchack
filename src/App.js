a649f418ef0f5ed792ff7440f67380ed8f476abf
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-18 04:53:09 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import {NavBar} from './components';
-import Maintenance from './pages/Maintenance/Maintenance';
 import NoMatch from './pages/NoMatch/NoMatch';
 import Landing from "./pages/Landing/Landing";
+// import Login from "./pages/Login/Login";
+import Register from "./pages/Register";
+
 
 function App() {
   return (
     <>
-
       <Router>
-          <NavBar />
           <Suspense fallback={<div>Loading....</div>}>
 
             <Routes>
 
               <Route exact path='/' element={<Landing />} />\
+              <Route exact path='register' element={<Register />} />\             
+              {/* <Route exact path='signin' element={<Login />} />\ */}
               <Route path='*' element={<NoMatch /> }/>
 
             </Routes>
