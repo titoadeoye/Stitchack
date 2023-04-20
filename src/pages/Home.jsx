05a914ceb4e8b574101bd9e15073c52a617c1916
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-20 15:28:06 -0800

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export default function Home () {
+    return (
+        <Fullpage>
+            <h2>Welcome</h2>
+        </Fullpage>
+    )
+};
+
+const Fullpage = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-color: #fafafa;
+`
