ac61d64685db3659909e76846141392a34930219
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-20 15:29:20 -0800

    Add files via upload

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,152 @@
 import styled from "styled-components";
-import {CustomInput} from "../components";
-import { Link } from "react-router-dom";
+import { CustomInput, Loader } from "../components";
+import { Link, useNavigate } from "react-router-dom";
+import { fireSwalError } from "../constants";
+import { signUp } from "../api/auth";
+import { useMutation } from "react-query";
+import * as Yup from "yup";
+import "yup-phone-lite";
+import { Formik } from "formik";
 
 
 export default function Register() {
+    const navigate = useNavigate();
+    const { isLoading: loading, mutate } = useMutation(signUp, {
+        onSuccess: () => {
+            navigate("/signin");
+        },
+        onError: (error) => {
+            fireSwalError(error.message);
+        },
+    });
+
+    const initialValues = {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        iAgree: false,
+    };
+    const validationSchema = Yup.object().shape({
+        firstname: Yup.string().required("Please enter your first name"),
+        lastname: Yup.string().required("Please enter your last name"),
+        email: Yup.string()
+            .email("Please enter a valid email")
+            .required("Please enter your email"),
+        password: Yup.string()
+            .required("Please enter your password")
+            .matches(
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})",
+                "Your password isn't strong enough"
+            ),
+        iAgree: Yup.boolean()
+            .required("The terms and conditions must be accepted.")
+            .oneOf([true], "You need to agree with our T&Cs"),
+    });
+
+
     return (
-        <Wrapper>
-            <section>
+        <>
+            <Wrapper>
+                <section>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={(values) => {
+                            const {password, ...others} = values;
+                            const data = {...others, secret: password, phoneNumber: "0000000000"}
+                            mutate(data);
+                        }}
+                    >
+                        {({
+                            values,
+                            handleSubmit,
+                            handleChange,
+                            handleBlur,
+                            errors,
+                            touched,
+                        }) => (
+                            <Form onSubmit={handleSubmit}>
+                                <h2>Sign Up</h2>
+                                <CustomInput
+                                    name="firstname"
+                                    value={values.firstname}
+                                    onChange={handleChange}
+                                    placeholder="First name"
+                                    onBlur={handleBlur}
+                                    error={errors.firstname}
+                                    touched={touched.firstname}
+                                />
+                                <CustomInput
+                                    name="lastname"
+                                    value={values.lastname}
+                                    onChange={handleChange}
+                                    placeholder="Last Name"
+                                    onBlur={handleBlur}
+                                    error={errors.lastname}
+                                    touched={touched.lastname}
+                                />
+                                <CustomInput
+                                    name="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    placeholder="Email"
+                                    type="email"
+                                    onBlur={handleBlur}
+                                    error={errors.email}
+                                    touched={touched.email}
+                                />
+                                <CustomInput
+                                    name="password"
+                                    value={values.password}
+                                    autoComplete="on"
+                                    onChange={handleChange}
+                                    placeholder="Password"
+                                    type="password"
+                                    onBlur={handleBlur}
+                                    error={errors.password}
+                                    touched={touched.password}
+                                />
+                                {touched.password && errors.password ? (
+                                    <Row>
+                                        <span>Passwords must have: </span>
+                                        <span className="purple">
+                                            8 characters, one digit and one symbol.
+                                        </span>
+                                    </Row>
+                                ) : null}
 
-                <Form>
-                    <h2>Sign Up</h2>
-                    <CustomInput type="text" placeholder="First name" required />
-                    <CustomInput type="text" placeholder="Last name" required/>
-                    <CustomInput type="email" placeholder="Email" required />
-                    <CustomInput type="password" placeholder="Password" required />
-                    <Button type="submit">Submit</Button>
-                    <Text>Already have an account?
-                        <Link to="/signin">  Sign in</Link>
-                    </Text>
-                </Form>
+                                <CustomInput
+                                    name="iAgree"
+                                    checked={values.iAgree}
+                                    onChange={handleChange}
+                                    type="checkbox"
+                                    span={
+                                        <>
+                                            I agree with <Link to="/privacyPolicy" target="_blank">privacy</Link> and{" "}
+                                            <Link to="/privacyPolicy" target="_blank">policy</Link>
+                                        </>
+                                    }
+                                    onBlur={handleBlur}
+                                    error={errors.iAgree}
+                                    touched={touched.iAgree}
+                                    className="policy"
+                                />
+                                <Button type="submit">
+                                    {loading ? <Loader /> : "Sign up"}
+                                </Button>
 
-                {/* <div class="colored-curve"></div> */}
-            </section>
+                                <Text>Already have an account?
+                                    <Link to="/signin">  Sign in</Link>
+                                </Text>
 
-        </Wrapper>
+                            </Form>
+                        )}
+                    </Formik>
+                </section>
+            </Wrapper>
+        </>
+
     )
 }
 
@@ -124,4 +247,22 @@ const Text = styled.h6`
             text-decoration: underline;
         }
     }
+`;
+
+const Row = styled.div`
+	width: 100%;
+	margin-bottom: 20px;
+	text-align: center;
+
+
+	> * {
+		font-style: normal;
+		font-weight: 500;
+		font-size: 0.8rem;
+	}
+
+	.purple {
+		color: #2b0548;
+        font-weight: 600;
+	}
 `;
