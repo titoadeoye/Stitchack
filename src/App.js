aa69cf273c73d8febe826f70281dc155e9f6f338
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-20 15:29:13 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NoMatch from './pages/NoMatch/NoMatch';
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Maintenance from "./pages/Maintenance/Maintenance";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
               <Route exact path='/' element={<Landing />} />\
               <Route exact path='register' element={<Register />} />\             
               <Route exact path='signin' element={<Login />} />\
+              <Route exact path='app' element={<Maintenance />} />\
               <Route path='*' element={<NoMatch /> }/>
 
             </Routes>
