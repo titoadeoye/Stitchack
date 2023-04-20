32e91139c8930b270af351eb6b56bbb8a7a941be
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-20 15:28:26 -0800

    Add files via upload

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,128 @@
 import styled from "styled-components";
-import {CustomInput} from "../components";
-import { Link } from "react-router-dom";
+import { CustomInput, Loader } from "../components";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { fireSwalError, device, setToStorage } from "../constants";
+import { logIn } from "../api/auth";
+import { useMutation } from "react-query";
+import * as Yup from "yup";
+import "yup-phone-lite";
+import { Formik } from "formik";
+import { useUserContext } from '../context/UserContext';
 
 
 export default function Login() {
+    const [error, setError] = useState('');
+    const { setUser, setIsLoggedIn } = useUserContext();
+    const navigate = useNavigate();
+
+    const initialValues = {
+        email: "",
+        password: "",
+        remember: false,
+    };
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .email("Please enter a valid email")
+            .required("Please enter your email"),
+        password: Yup.string()
+            .required("Please enter your password")
+    });
+
+    const { isLoading: loading, mutate } = useMutation(logIn, {
+		onSuccess: (data) => {
+			setError('');
+			setToStorage("token", data.token);
+			setIsLoggedIn(data.token);
+			setToStorage("user", data.user);
+			setUser(data.user);
+            navigate("/app")
+		},
+		onError: (error) => {
+			console.log(error);
+            setError("Username/password is incorrect")
+            fireSwalError(error.message);
+			},
+	});
+
+
     return (
         <Wrapper>
             <section>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={(values) => {
+                        const { password, ...others } = values;
+                        const data = { ...others, secret: password }
+                        mutate(data);
+                    }}
+                >
+                    {({
+                        values,
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        errors,
+                        touched,
+                    }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <h2>Sign in</h2>
+                    
+                            <CustomInput
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                placeholder="Email"
+                                type="email"
+                                onBlur={handleBlur}
+                                error={errors.email}
+                                touched={touched.email}
+                            />
+                            <CustomInput
+                                name="password"
+                                value={values.password}
+                                autoComplete="on"
+                                onChange={handleChange}
+                                placeholder="Password"
+                                type="password"
+                                onBlur={handleBlur}
+                                error={errors.password}
+                                touched={touched.password}
+                            />
 
-                <Form>
+                            {error && <Error>{error}</Error>}
+                            <Row>
+                                <CustomInput
+                                    name="remember"
+                                    checked={values.remember}
+                                    onChange={handleChange}
+                                    type="checkbox"
+                                    span={"Remember me"}
+                                />
+                            </Row>
+                            <Button type="submit">
+                                {loading ? <Loader /> : "Sign in"}
+                            </Button>
+
+                            <Text>Don't have an account?
+                        <Link to="/register">  Sign up</Link>
+                    </Text>
+
+                        </Form>
+                    )}
+                </Formik>
+                {/* <Form>
                     <h2>Sign In</h2>
                     <CustomInput type="email" placeholder="Email" required />
                     <CustomInput type="password" placeholder="Password" required />
                     <Button type="submit">Submit</Button>
-                    <Text>Don't have an account?  
+                    <Text>Don't have an account?
                         <Link to="/register">  Sign up</Link>
                     </Text>
-                </Form>
+                </Form> */}
 
-                {/* <div class="colored-curve"></div> */}
             </section>
 
         </Wrapper>
@@ -66,6 +170,22 @@ const Wrapper = styled.div`
     }
 `;
 
+const Row = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	margin-bottom: 15px;
+	margin-top: 10px;
+	
+	@media ${device.mobileS} {
+		flex-direction: column;
+		justify-content: initial;
+		align-items: center;
+		margin-bottom: 20px;
+	}
+`;
+
 const Form = styled.form`
     width: 40%;
     margin: auto;
@@ -94,6 +214,14 @@ const Form = styled.form`
         }
     }
 `;
+
+const Error = styled.span`
+	padding: 5px;
+	color: #ff0000b8;
+    font-weight: 900;
+    font-size: 0.7em;
+    text-align: center;
+`
 
 const Button = styled.button`
 border: none;
