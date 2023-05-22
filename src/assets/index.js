a00dad394ea074b160cb30c394f7fddd0c92ee73
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-05-22 21:01:08 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import howItWorks from './how-it-works.png';
+
 import landing from './landing.png';
 import laptop from './laptop.png';
 import login from './login.png';
@@ -11,9 +11,34 @@ import bg1 from './bg1.png';
 import bg from './bg.png';
 import female from "./female.jpeg";
 import male from "./male.jpeg";
+import styles from "./style.png";
+import ankara from "./ankara.png";
+import BlackDress from "./black-dress.jpg";
+import blouse from "./blouse.webp";
+import chiffon from "./chiffon.png";
+import cotton from "./cotton.png";
+import damask from "./damask.png";
+import crepe from "./crepe.png";
+import lace from "./lace.png";
+
+import linen from "./linen.webp";
+import lycra from "./lycra.png";
+import satin from "./satin.png";
+import silk from "./silk.png";
+import skirt from "./skirt.webp";
+import top from "./top.png";
+import trouser from "./trouser.webp";
+import twoPiece from "./two-piece.jpg";
+
+import velvet from "./velvet.png";
+import wedding from "./wedding-dress.jpg";
+
+
+
+
 
 export {
-    howItWorks,
+
     landing,
     laptop,
     login,
@@ -25,5 +50,23 @@ export {
     bg,
     bg1,
     female,
-    male
+    male,
+    ankara,
+    BlackDress,
+    chiffon,
+    crepe,
+    cotton,
+    blouse,
+    damask,
+    lace,
+    linen,
+    lycra,
+    satin,
+    silk,
+    skirt,
+    top,
+    trouser,
+    twoPiece,
+    velvet,
+    wedding
 }
